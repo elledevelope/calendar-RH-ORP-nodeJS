703db257e9ad44dcca647b8c7f2b16e4559374dd
elmira <elledevelope@gmail.com>
2024-11-06 12:39:35 +0100

    Created loop ``for`` on `salaries_count`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 const io = require('socket.io')(server); //172.20.192.227
-const { faker } = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker'); // faker
 
 const ip = "127.0.0.1";
 const port = 4000;
@@ -13,8 +13,20 @@ app.get('/',(req,res)=>{
 })
 
 
+// faker
 const salaries = [];
 const salaries_count = 15;
+//id_salarie : faker.string.alphanumeric(5) // '3e5V7'
+// fullName_salarie : faker.person.fullName()
+//lastName_salarie : faker.person.lastName()
+
+for (let index = 0; index < salaries_count; index++) {
+    console.log(index); 
+}
+
+
+
+
 
 
 
